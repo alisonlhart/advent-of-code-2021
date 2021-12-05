ea2c72fc4d1b2014d112ce700a745592e3b5af83
alisonlhart <contact@alisonlhart.com>
2021-12-05 17:30:11 -0500

    adv-of-code day 1 src

---
+++ day_1/day1_main.py
@@ -0,0 +1,38 @@
+""" advent of code - day 1 
+    author: @alisonlhart
+"""
+
+"""
+
+Goal: Count number of increases from list element to element 
+Challenge: Don't do it in a forloop (maybe a while...)
+Idea:
+- Read the file into a list (turn string into int)
+- set comparison var and increase var to 0
+- iterate through list, skip at index 0, compare to item, 
+- if item is greater than comparison var, add 1 to increase var
+- set comparison var value to current item
+- repeat until 0
+
+"""
+
+comparison_var=0
+increase_var=0
+
+def comparison(list_element, compare_var) -> int:
+    return 1 if list_element > compare_var else 0
+
+
+
+with open("input.txt") as file:
+    for value in file:
+        value=value.strip()
+        value=int(value)
+        if (comparison_var!=0 and comparison(value,comparison_var)):
+            print(str(value)+" (INCREASED)\n")
+            increase_var+=1
+        else:
+            print(str(value)+" (DECREASED)\n")
+        comparison_var=value
+
+    print("Total: "+str(increase_var))
